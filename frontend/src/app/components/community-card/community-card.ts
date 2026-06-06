937f692f2c9a020daca913e5befe24bbe027225d
martina <martujoaquin02@gmail.com>
2026-06-06 15:58:28 -0300

    navbar inicial angular

---
+++ frontend/src/app/components/community-card/community-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-community-card',
+  imports: [],
+  templateUrl: './community-card.html',
+  styleUrl: './community-card.css',
+})
+export class CommunityCard {
+
+}
---
+++ frontend/src/app/components/staff-card/staff-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-staff-card',
+  imports: [],
+  templateUrl: './staff-card.html',
+  styleUrl: './staff-card.css',
+})
+export class StaffCard {
+
+}
