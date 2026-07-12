aa8d9db0bced276dfb4503b697c28d950fcac5d7
martina <martujoaquin02@gmail.com>
2026-07-12 19:46:30 -0300

    staff + aboutUs

---
+++ frontend/src/app/components/about-section/about-section.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about-section',
+  templateUrl: './about-section.html',
+  styleUrl: './about-section.css',
+})
+export class AboutSection {}
--- frontend/src/app/pages/home/home.ts
+++ frontend/src/app/pages/home/home.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { Navbar } from '../../components/navbar/navbar';
 import { Hero } from '../../components/hero/hero';
+import { AboutSection } from '../../components/about-section/about-section';
 import { ServicesSection } from '../../components/services-section/services-section';
 import { StaffSection } from '../../components/staff-section/staff-section';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [Navbar, Hero, ServicesSection, StaffSection],
+  imports: [Navbar, Hero, AboutSection, ServicesSection, StaffSection],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
